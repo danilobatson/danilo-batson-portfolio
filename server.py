0e3d64285b8bff203943d52c7b623e507e4ae0cb
danilobatson <djbatson19@gmail.com>
2022-12-21 01:12:04 -0800

    adds post link to message box

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 '''This is the main entry point for the web server.'''
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,13 @@ app = Flask(__name__)
 def home_page():
     '''Renders home page.'''
     return render_template("index.html")
+
+@app.route("/submit_form", methods=["GET", "POST"])
+def submit_form():
+    '''Submits contact form.'''
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        return 'form submitted successfully'
+    else:
+        return 'something went wrong'
