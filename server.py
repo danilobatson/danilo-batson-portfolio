1d5cd0b73b1e254ddfb09f49703081baf56b2c74
danilobatson <djbatson19@gmail.com>
2022-12-21 01:21:19 -0800

    creates thank you page

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 '''This is the main entry point for the web server.'''
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home_page():
     '''Renders home page.'''
     return render_template("index.html")
+@app.route("/thankyou.html")
+def thank_you():
+    '''Renders thank you page.'''
+    return render_template("thank_you.html")
 
 @app.route("/submit_form", methods=["GET", "POST"])
 def submit_form():
@@ -14,6 +18,6 @@ def submit_form():
     if request.method == "POST":
         data = request.form.to_dict()
         print(data)
-        return 'form submitted successfully'
+        return redirect("/thankyou.html")
     else:
         return 'something went wrong'
