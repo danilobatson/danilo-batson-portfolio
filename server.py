58f810fd483aa662d429a1736acbb556ff7b750e
danilobatson <djbatson19@gmail.com>
2022-12-20 22:59:10 -0800

    starts off site

---
+++ server.py
@@ -0,0 +1,10 @@
+'''This is the main entry point for the web server.'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    '''Renders home pagee.'''
+    return render_template("index2.html", name="home")
+
